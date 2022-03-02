a8a82d1d5e1e17bf8960c248af18858f204a1346
Sauraj <saureyj43@gmail.com>
2022-03-02 03:11:53 +0100

    Add files via upload

--- RL/Flappy Bird/flappy_bird.py
+++ RL/Flappy Bird/flappy_bird.py
@@ -5,6 +5,30 @@ import time
  
 import text_flappy_bird_gym
 
+class QLearning:
+
+    def __init__(self,environment,alpha,gamma,epsilon):
+
+        self.actions = environment.action_space.n
+        self.alpha = alpha 
+        self.gamma = gamma 
+        self.epsilon = epsilon 
+        self.Q_table = {} #Key: Coordinate position (State), Value: Action
+
+
+    def initialize_q_values(self,state):
+        #Check if the state exists in the table 
+        #If not, then load in the initialized value as 0
+        if state in self.Q_table is None:
+            self.Q_table[state] = [0,0]
+
+    # def agent_start(self,state):
+
+    #     #Choose a random action using epsilon-greedy strategy 
+    #     random_action_prob = np.random.random()
+    #     if random_action_prob < self.epsilon:
+            #Choose a random action to explore 
+
 if __name__ == '__main__':
 
     # initiate environment
@@ -19,6 +43,7 @@ if __name__ == '__main__':
 
         # Appy action and return new observation of the environment
         obs, reward, done, info = env.step(action)
+        print(reward)
 
         # Render the game
         os.system("clear")
